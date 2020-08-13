597c63c2e127f2ea3345603cc177a3f289355cb5
Henley Kuang <hkuang@brightedge.com>
2020-08-12 18:09:44 -0700

    add more examples

--- src/app2.py
+++ src/app2.py
@@ -19,20 +19,9 @@ def create_signal_handlers(exit_flag):
 def _main():
     exit_flag = threading.Event()
     create_signal_handlers(exit_flag)
-    thread_count = 5
-
-    def worker(exit_flag):
-        while exit_flag.is_set() is False:
-            LOGGER.info("thread alive: %s", exit_flag.is_set())
-            exit_flag.wait(5)
-        LOGGER.info("exit_flag set")
-
-    for _ in range(thread_count):
-        t = threading.Thread(target=worker, args=(exit_flag,))
-        t.start()
 
     while exit_flag.is_set() is False:
-        time.sleep(2)
+        time.sleep(5)
         LOGGER.info("...")
 
 
